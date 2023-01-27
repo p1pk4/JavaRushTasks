48c4e0cffd82cc38129674ebdea0aa9d17604ca5
ToxicBugsy <pipkagr@gmail.com>
2023-01-27 11:48:20 +0200

    task done

--- 1.JavaSyntax/src/com/javarush/task/jdk13/task04/task0441/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/jdk13/task04/task0441/Solution.java
@@ -8,19 +8,36 @@ import java.io.InputStreamReader;
 */
 
 public class Solution {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception
+    {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        int one = Integer.parseInt(reader.readLine());
-        int two = Integer.parseInt(reader.readLine());
-        int three = Integer.parseInt(reader.readLine());
-        
-        int coutnOne = 0;
+        int a = Integer.parseInt(reader.readLine());
+        int b = Integer.parseInt(reader.readLine());
+        int c = Integer.parseInt(reader.readLine());
 
-
-        if (one > two){
-            coutnOne = one;
-            System.out.println(coutnOne);
+        if(((a > b) & (a < c)) | ((a < b) & (a > c)))
+        {
+            System.out.println(a);
         }
-
+        if(((b > a) & (b < c)) | ((b < a) & (b > c)))
+        {
+            System.out.println(b);
+        }
+        if(((c > a) & (c < b)) |(c < a) & (c > b))
+        {
+            System.out.println(c);
+        }
+        else if ((a == b) | (b == c))
+        {
+            System.out.println(b);
+        }
+        else if (a == c)
+        {
+            System.out.println(a);
+        }
+        else if ((a == b) & (b == c) & (a == c))
+        {
+            System.out.println(c);
+        }
     }
 }
