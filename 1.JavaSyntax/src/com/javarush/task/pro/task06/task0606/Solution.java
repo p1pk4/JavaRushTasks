0a75b13fc4235c53f8d35b729ae7f0bf142e6dfe
ToxicBugsy <pipkagr@gmail.com>
2023-02-05 14:22:21 +0200

    Task completed

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0606/Solution.java
@@ -0,0 +1,45 @@
+package com.javarush.task.pro.task06.task0606;
+
+/* 
+Универсальный солдат
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void universalMethod(){
+
+    }
+
+    public static void universalMethod(int a){
+
+    }
+    public static void universalMethod(double ds, char dfd){
+
+    }
+    public static void universalMethod(char f){
+
+    }
+    public static void universalMethod(int a, float d){
+
+    }
+    public static void universalMethod(int a, String s){
+
+    }
+    public static void universalMethod(int a, int b){
+
+    }
+
+    public static void universalMethod(double a){
+
+    }
+    public static void universalMethod(float a){
+
+    }
+    public static void universalMethod(boolean a){
+
+    }
+}
