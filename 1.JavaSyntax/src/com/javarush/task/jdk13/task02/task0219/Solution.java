e2ee504581d0378cbc8efd16fb31cb900f31901d
ToxicBugsy <pipkagr@gmail.com>
2023-02-09 13:02:32 +0200

    Task completed

---
+++ 1.JavaSyntax/src/com/javarush/task/jdk13/task02/task0219/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.jdk13.task02.task0219;
+
+/* 
+Вывод текста на экран
+*/
+
+public class Solution {
+    public static void print3(String text) {
+        for (int i = 0; i < 3; i++) {
+            System.out.print(text + " ");
+        }
+
+    }
+
+    public static void main(String[] args) {
+        print3("window");
+        print3("file");
+    }
+}
