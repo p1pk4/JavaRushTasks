3755445b8654622b34ae134e689b842139cd7d5b
ToxicBugsy <pipkagr@gmail.com>
2023-02-14 16:54:36 +0200

    Task completed

---
+++ 1.JavaSyntax/src/com/javarush/task/jdk13/task02/task0208/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.jdk13.task02.task0208;
+
+/* 
+Коты - это хорошо
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Cat catOne = new Cat();
+        Cat catTwo = new Cat();
+    }
+
+    public static class Cat {
+
+    }
+}
