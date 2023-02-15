bd8a2edf26f97473f0239e5e098f8aa326aaee03
ToxicBugsy <pipkagr@gmail.com>
2023-02-15 22:16:11 +0200

    Task completed

---
+++ 1.JavaSyntax/src/com/javarush/task/jdk13/task02/task0223/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.jdk13.task02.task0223;
+
+/* 
+По разному
+*/
+
+public class Solution {
+    int a = 1;
+    int b = 2;
+
+    String g, gg;
+
+    float aa = 12, dd = 23;
+}
